fa6d1a39a74d00925c1e2d06ea8f98350370deff
Hunter Ritchie <hritchie@redhat.com>
2013-06-05 11:56:16 -0400

    grabbed support files from brms customer demo to make config of brms with torquebox easier, added rails files

---
+++ src/main/ruby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruby::Application.config.secret_token = '09a51272f3d23120ef9232569eb84b9fa78817ee80a752fb8ad481e2e71f9450952a90679e52fb4deb4d9df0e986c92e3a9f2a8bcb69b5f835d29c8b6e7ea83c'
